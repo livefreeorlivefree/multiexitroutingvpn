ec59005fb180d471e32a9cc72ef10ef59985e6ec
zhar-ptica admin <admin@zhar-ptica>
2015-09-18 14:29:16 +0300

    So it begins

---
+++ vars/environment.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BASE="192.168.0"
+PING_HOST="bbc.co.uk"
+DOWNLOAD_TEST_FILE="https://github.com/livefreeorlivefree/stuff/raw/master/.testfile"
+TIMEOUT_VALUE=20 #seconds
+
+SCRIPT_DIR="/sbin/mevr"
+CONFIG_DIR="/etc/merv" #could be etc tho
+LOG_DIR="/var/log/merv" #could be var/log/dudos/ tho
+
+
+MARK_CONST=1024 #eh.
+
+PERSIST_OPTS="--persist-local-ip"
+
+### prog locations
+ifconfig="/sbin/ifconfig"
+ip="/sbin/ip"
+iptables="/sbin/iptables"
+openvpn="/usr/sbin/openvpn"
